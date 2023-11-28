2a151f55a79bed0cec9c761eaeea879f2b4c7c24
MeesterDavid <david.isaacspaternostro@hu.nl>
2023-11-28 15:10:37 +0100

    functional priority_queue

--- priority_queue.py
+++ priority_queue.py
@@ -1,38 +1,44 @@
 import math
+import sys
+
+
 """" 
 https://medium.com/@verdi/binary-heap-basics-40a0f3f41c8f
 """
 
-def get_parent(index):
+
+def get_parent(pq, index):
     if index == 0:
         return None
     else:
         return math.floor((index-1)/2)
 
 
-def get_child(index):
-    if index > len(index):
-        return None, None
+def get_child(pq, index):
+    if index >= len(pq):
+        return None
     else:
         return index
 
 
-def get_left_child(index):
+def get_left_child(pq, index):
     """returns index, value"""
     left_child_index = 2*index+1
-    return get_child(left_child_index)
+    return get_child(pq, left_child_index)
 
 
 def get_right_child(pq, index):
     right_child_index = 2*index+2
-    return get_child(right_child_index)
+    return get_child(pq, right_child_index)
 
 
-def compare(v1, v2, comparator=lambda x,y: x < y):
-    if v2 is None:
+def _right_child_index(index):
+    return 2*index+2
+
+
+def compare(v1, v2, comparator=lambda x,y: x < y) -> bool:
+    if v1 is None or v2 is None:
         return False
-    elif v1 is None:
-        return True
     else:
         return comparator(v1, v2)
 
@@ -43,7 +49,7 @@ def get_smallest_child(pq, index):
     if left is None:
         return None
     elif right is None:
-        return right
+        return left
     elif compare(pq[left], pq[right]):
         return left
     else:
@@ -62,11 +68,11 @@ def pop(pq):
     result = pq[0]
 
     current_index = 0
-    pq[0] = pq.pop(-1)
+    pq[0] = pq.pop(-1)  # this is built-in list pop
 
     smallest_child = get_smallest_child(pq, current_index)
 
-    while smallest_child is not None and compare(pq[current_index], pq[smallest_child]):
+    while smallest_child is not None and not compare(pq[current_index], pq[smallest_child]):
         swap(pq, current_index, smallest_child)
         current_index = smallest_child
         smallest_child = get_smallest_child(pq, current_index)
@@ -77,12 +83,12 @@ def pop(pq):
 def insert(pq, value):
     pq.append(value)
     current_index = len(pq)-1
-    parent = get_parent(current_index)
+    parent = get_parent(pq, current_index)
 
-    while parent is not None and compare(pq, pq[current_index], pq[parent]):
+    while parent is not None and compare(pq[current_index], pq[parent]):
         swap(pq, current_index, parent)
         current_index = parent
-        parent = get_parent(current_index)
+        parent = get_parent(pq, current_index)
 
 
 def from_list(lst):
@@ -90,4 +96,6 @@ def from_list(lst):
 
     for e in lst:
         insert(pq, e)
+
+    return pq
 
